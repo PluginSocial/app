207dcd2803c2810bbff9bdd7f105e2e73124cf0d
SOSTheBlack <jeancesargarcia@gmail.com>
2022-05-06 11:37:44 -0300

    install livewire/livewire

--- resources/views/layouts/verticalLayoutMaster.blade.php
+++ resources/views/layouts/verticalLayoutMaster.blade.php
@@ -1,65 +1,66 @@
 <body
-  class="{{$configData['mainLayoutTypeClass']}} @if(!empty($configData['bodyCustomClass']) && isset($configData['bodyCustomClass'])) {{$configData['bodyCustomClass']}} @endif @if($configData['isMenuCollapsed'] && isset($configData['isMenuCollapsed'])){{'menu-collapse'}} @endif"
-  data-open="click" data-menu="vertical-modern-menu" data-col="2-columns">
+    class="{{$configData['mainLayoutTypeClass']}} @if(!empty($configData['bodyCustomClass']) && isset($configData['bodyCustomClass'])) {{$configData['bodyCustomClass']}} @endif @if($configData['isMenuCollapsed'] && isset($configData['isMenuCollapsed'])){{'menu-collapse'}} @endif"
+    data-open="click" data-menu="vertical-modern-menu" data-col="2-columns">
 
-  <!-- BEGIN: Header-->
-  <header class="page-topbar" id="header">
+<!-- BEGIN: Header-->
+<header class="page-topbar" id="header">
     @include('panels.navbar')
-  </header>
-  <!-- END: Header-->
+</header>
+<!-- END: Header-->
 
-  <!-- BEGIN: SideNav-->
-  @include('panels.sidebar')
-  <!-- END: SideNav-->
+<!-- BEGIN: SideNav-->
+@include('panels.sidebar')
+<!-- END: SideNav-->
 
-  <!-- BEGIN: Page Main-->
-  <div id="main">
+<!-- BEGIN: Page Main-->
+<div id="main">
     <div class="row">
-      @if ($configData["navbarLarge"] === true)
-        @if(($configData["mainLayoutType"]) === 'vertical-modern-menu')
-        {{-- navabar large  --}}
-        <div
-          class="content-wrapper-before @if(!empty($configData['navbarBgColor'])) {{$configData['navbarBgColor']}} @else {{$configData["navbarLargeColor"]}} @endif">
-        </div>
-        @else
-        {{-- navabar large  --}}
-        <div class="content-wrapper-before {{$configData["navbarLargeColor"]}}">
-        </div>
+        @if ($configData["navbarLarge"] === true)
+            @if(($configData["mainLayoutType"]) === 'vertical-modern-menu')
+                {{-- navabar large  --}}
+                <div
+                    class="content-wrapper-before @if(!empty($configData['navbarBgColor'])) {{$configData['navbarBgColor']}} @else {{$configData["navbarLargeColor"]}} @endif">
+                </div>
+            @else
+                {{-- navabar large  --}}
+                <div class="content-wrapper-before {{$configData["navbarLargeColor"]}}">
+                </div>
+            @endif
         @endif
-      @endif
 
 
-      @if($configData["pageHeader"] === true && isset($breadcrumbs))
-      {{--  breadcrumb --}}
-      @include('panels.breadcrumb')
-      @endif
-      <div class="col s12">
-        <div class="container">
-          {{-- main page content --}}
-          @yield('content')
-          {{-- right sidebar --}}
-          @include('pages.sidebar.right-sidebar')
-          @if($configData["isFabButton"] === true)
-            @include('pages.sidebar.fab-menu')
-          @endif
+        @if($configData["pageHeader"] === true && isset($breadcrumbs))
+            {{--  breadcrumb --}}
+            @include('panels.breadcrumb')
+        @endif
+        <div class="col s12">
+            <div class="container">
+                {{-- main page content --}}
+                @yield('content')
+                {{-- right sidebar --}}
+                @include('pages.sidebar.right-sidebar')
+                @if($configData["isFabButton"] === true)
+                    @include('pages.sidebar.fab-menu')
+                @endif
+            </div>
+            {{-- overlay --}}
+            <div class="content-overlay"></div>
         </div>
-        {{-- overlay --}}
-        <div class="content-overlay"></div>
-      </div>
     </div>
-  </div>
-  <!-- END: Page Main-->
+</div>
+<!-- END: Page Main-->
 
 
-  @if($configData['isCustomizer'] === true)
-  <!-- Theme Customizer -->
-  @include('pages.partials.customizer')
-  <!--/ Theme Customizer -->
-  @endif
+@if($configData['isCustomizer'] === true)
+    <!-- Theme Customizer -->
+    @include('pages.partials.customizer')
+    <!--/ Theme Customizer -->
+@endif
 
 
-  {{-- footer  --}}
-  @include('panels.footer')
-  {{-- vendor and page scripts --}}
-  @include('panels.scripts')
+{{-- footer  --}}
+@include('panels.footer')
+{{-- vendor and page scripts --}}
+@include('panels.scripts')
+@livewireScripts
 </body>
